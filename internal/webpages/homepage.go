29275055bb7474d636446f34c9a7303936429e66
Henrique Vicente <henriquevicente@gmail.com>
2020-08-02 22:48:06 +0200

    webpages: adding breadcrumb to product page example.

--- internal/webpages/homepage.go
+++ internal/webpages/homepage.go
@@ -16,6 +16,12 @@ func (h *HomepageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	resp := &HTMLResponse{
 		Template: "homepage",
 		Title:    "homepage",
+		Breadcrumb: []Breadcrumb{
+			{Text: "Computers", Link: "/c/computers"},
+			{Text: "Displays", Link: "/c/computers-displays"},
+			{Text: "4K", Link: "/c/computers-displays-4k"},
+			{Text: "LG Ultrafine 24\" 4K", Active: true},
+		},
 	}
 	h.Frontend.Respond(w, r, resp)
 }
