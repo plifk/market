36d866e909ecb5879ca0677d49414b5d98eb6838
Henrique Vicente <henriquevicente@gmail.com>
2020-08-16 01:42:50 +0200

    ci: adding GitHub Actions tasks and scripts for linting and testing code.

--- go.mod
+++ go.mod
@@ -9,8 +9,12 @@ require (
 	github.com/henvic/clino v0.0.1
 	github.com/henvic/ctxsignal v1.0.0
 	github.com/henvic/httpretty v0.0.5
+	github.com/jackc/pgconn v1.6.4
+	github.com/jackc/pgerrcode v0.0.0-20190803225404-afa3381909a6
 	github.com/jackc/pgx/v4 v4.8.1
 	github.com/justinas/nosurf v1.1.0
 	github.com/nyaruka/phonenumbers v1.0.57
+	github.com/securego/gosec v0.0.0-20200401082031-e946c8c39989 // indirect
 	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	mvdan.cc/unparam v0.0.0-20200501210554-b37ab49443f7 // indirect
 )
--- scripts/ci-lint.sh
+++ scripts/ci-lint.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-set -euo pipefail
+set -xeuo pipefail
 IFS=$'\n\t'
 
 # Static analysis scripts
 cd $(dirname $0)/..
 
 source scripts/lib.sh
-ensure_go_binary golang.org/x/lint/golint
 ensure_go_binary honnef.co/go/tools/cmd/staticcheck
 ensure_go_binary github.com/securego/gosec/cmd/gosec
 ensure_go_binary github.com/client9/misspell/cmd/misspell
+ensure_go_binary mvdan.cc/unparam
 
-echo "Running golint."
-test -z "$(golint `go list ./...` | tee /dev/stderr)"
-
-echo "Running go vet."
+source scripts/ci-lint-fmt.sh
 go vet -all ./...
-
-echo "Running staticcheck toolset."
 staticcheck ./...
-
-echo "Running tosec to check possible security issues."
 gosec -quiet ./...
-
-echo "Running mispell."
 misspell cmd/**/*.{go,sh} internal/**/*.{go} README.md
-
-echo "Running unparam."
 unparam ./...
-
-echo "Running tests with data race detector"
-go test -race ./...
